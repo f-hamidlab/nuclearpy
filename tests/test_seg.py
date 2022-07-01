18cedf5e44643058a03488b46cb5c842ea3e0f02
Fursham Hamid <fursham.h@gmail.com>
2022-07-01 15:25:24 +0100

    switch off GPU for testing

--- tests/test_seg.py
+++ tests/test_seg.py
@@ -4,6 +4,6 @@ import shutil
 from ngtools.wrappers import runNGS
 
 def test_runngs():
-	runNGS("data/sample_data/induced", channels=["dapi", "beta3", "rfp", "ngn"], outdir="data/test_out")
+	runNGS("data/sample_data/induced", channels=["dapi", "beta3", "rfp", "ngn"], outdir="data/test_out", useGPU=False)
 	assert filecmp.cmp("data/sample_output/out_ng/output.csv", 'data/test_out/induced/out_ng/output.csv'), "test failed"
 	shutil.rmtree("data/test_out")
