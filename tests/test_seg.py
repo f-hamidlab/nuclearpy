28d6007c098514767b3d0b82951a151fedf048f0
Fursham Hamid <fursham.h@gmail.com>
2022-07-12 16:47:15 +0100

    Add todo

--- tests/test_seg.py
+++ tests/test_seg.py
@@ -3,6 +3,8 @@ import filecmp
 import shutil
 from ngtools.wrappers import runNGS
 
+# TODO: Update test outputs
+# TODO: Create more robust tests
 def test_runngs():
 	runNGS("data/sample_data/induced", channels=["dapi", "beta3", "rfp", "ngn"], outdir="data/test_out", useGPU=False)
 	assert filecmp.cmp("data/sample_output/out_ng/output.csv", 'data/test_out/induced/out_ng/output.csv'), "test failed"
